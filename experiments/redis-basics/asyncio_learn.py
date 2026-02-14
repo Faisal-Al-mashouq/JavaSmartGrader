59c1867e470fb34ddf3f549c8d81913d613c61a7
Faisal-Al-mashouq <faisalmashouq@gmail.com>
2026-02-14 22:40:40 +0300

    Adding experiments to test Redis and Asyncio. Some additions to sandbox.py and sandbox_worker.py + fixing linters

---
+++ experiments/redis-basics/asyncio_learn.py
@@ -0,0 +1,12 @@
+import asyncio
+
+async def greet():
+    asyncio.timeout(2)
+    print("Hello, World!")
+
+async def meet():
+    print("Nice to meet you!")
+
+if __name__ == "__main__":
+    asyncio.run(greet())
+    asyncio.run(meet())
